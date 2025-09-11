aed73bc7230f8dd73240bda6d3175a3d118ab03a
derek-ak <derek@dw.capital>
2025-09-11 06:08:26 -0400

    Fix migration class name with proper timestamp

--- backend/src/database/migrations/008-add-tester-fields-to-users.ts
+++ backend/src/database/migrations/008-add-tester-fields-to-users.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export class AddTesterFieldsToUsers008 implements MigrationInterface {
-  name = 'AddTesterFieldsToUsers008';
+export class AddTesterFieldsToUsers0081700000008 implements MigrationInterface {
+  name = 'AddTesterFieldsToUsers0081700000008';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     // Add isTester column
